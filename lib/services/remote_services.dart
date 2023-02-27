4db4d54351fd8d402b3426e6a4478b703788c12f
Priyanshu <2003priyanshusingh@gmail.com>
2023-02-27 22:05:25 +0530

    Finall app completed

---
+++ lib/services/remote_services.dart
@@ -0,0 +1,15 @@
+import 'package:integrated_flutter/models/post.dart';
+import 'package:http/http.dart' as http;
+
+class RemoteService {
+  Future<List<Welcome>?> getPosts() async {
+    var clint = http.Client();
+
+    var uri = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+    var response = await clint.get(uri);
+    if (response.statusCode == 200) {
+      var json = response.body;
+      return welcomeFromJson(json);
+    }
+  }
+}
